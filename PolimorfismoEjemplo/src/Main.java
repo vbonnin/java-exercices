3bf286a606c3df10b0ef268dc5542779b3239c31
vbonnin <vbonninpalou@cifpfbmoll.eu>
2023-03-23 16:01:11 +0100

    ejemplos polimorfismo

---
+++ PolimorfismoEjemplo/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehiculo miVehiculo = new Coche();
+        miVehiculo.mover(15); // El coche está avanzando 15 metros
+        // El coche está yendo rápido
+        miVehiculo = new Bicicleta();
+        miVehiculo.mover(3); // La bicicleta está pedaleando 3 metros
+        // La bicicleta está yendo lento
+    }
+}
